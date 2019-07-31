69d6ebf37d17730634c03386272756d706f5806f
Radu M <root@radu.sh>
2019-07-31 23:06:31 +0300

    Add Brigade script and deps
    Signed-off-by: Radu M <root@radu.sh>

---
+++ brigade.js
@@ -0,0 +1,27 @@
+const { events, Job } = require("@brigadecore/brigadier");
+const { Check } = require("@brigadecore/brigade-utils");
+
+const projectName = "brigade-utils";
+const jsImg = "node:12.3.1-stretch";
+
+
+function build(e, project) {
+    var build = new Job(`${projectName}-build`, jsImg);
+
+    build.tasks = [
+
+        "ls -al"
+    ];
+
+    return build;
+
+}
+
+function runSuite(e, p) {
+    var check = new Check(e, p, build());
+    check.run();
+}
+
+events.on("check_suite:requested", runSuite);
+events.on("check_suite:rerequested", runSuite);
+events.on("exec", runSuite);
