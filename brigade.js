9a4ca12b5d67c42bee53183cc8ec507008e81dba
Radu M <root@radu.sh>
2019-08-05 12:01:21 +0300

    Testing the kind library
    Signed-off-by: Radu M <root@radu.sh>

--- brigade.js
+++ brigade.js
@@ -1,11 +1,11 @@
 const { events, Job } = require("@brigadecore/brigadier");
-const { Check } = require("@brigadecore/brigade-utils");
+const { Check, Kind } = require("@brigadecore/brigade-utils");
 
 const projectName = "brigade-utils";
 const jsImg = "alpine";
 
 
-function build(e, project) {
+function build(event, project) {
     var build = new Job(`${projectName}-build`, jsImg);
 
     build.tasks = [
@@ -14,12 +14,50 @@ function build(e, project) {
     ];
 
     return build;
-
 }
 
+function e2e(event, project) {
+    var kind = new Kind();
+    kind.tasks.push("kubectl get pods --all-namespaces");
+
+    return kind;
+}
+
+
+// Here we can add additional Check Runs, which will run in parallel and
+// report their results independently to GitHub
 function runSuite(e, p) {
-    var check = new Check(e, p, build());
-    check.run();
+    // For the master branch, we build and publish images in response to the push
+    // event. We test as a precondition for doing that, so we DON'T test here
+    // for the master branch.
+    if (e.revision.ref != "master") {
+        // Important: To prevent Promise.all() from failing fast, we catch and
+        // return all errors. This ensures Promise.all() always resolves. We then
+        // iterate over all resolved values looking for errors. If we find one, we
+        // throw it so the whole build will fail.
+        //
+        // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Promise.all_fail-fast_behaviour
+        //
+        // Note: as provided language string is used in job naming, it must consist
+        // of lowercase letters and hyphens only (per Brigade/K8s restrictions)
+        return Promise.all([
+            runTests(e, p, build).catch((err) => { return err }),
+            runTests(e, p, e2e).catch((err) => { return err }),
+        ])
+            .then((values) => {
+                values.forEach((value) => {
+                    if (value instanceof Error) throw value;
+                });
+            });
+    }
+}
+
+// runTests is a Check Run that is run as part of a Checks Suite
+function runTests(e, p, jobFunc) {
+    console.log("Check requested");
+
+    var check = new Check(e, p, jobFunc());
+    return check.run();
 }
 
 events.on("check_suite:requested", runSuite);
