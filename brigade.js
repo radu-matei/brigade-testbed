94e3a79bf7081f7e0eaeca23ed6af366ed190be3
Radu M <root@radu.sh>
2019-08-09 22:13:42 +0300

    Let's hope this doesn't crash my cluster...
    Signed-off-by: Radu M <root@radu.sh>

--- brigade.js
+++ brigade.js
@@ -7,14 +7,20 @@ const jsImg = "node:12.3.1-stretch";
 function e2e(e, project) {
     let kind = new KindJob("kind");
     kind.tasks.push(
-        "wget https://get.helm.sh/helm-v3.0.0-alpha.2-linux-amd64.tar.gz",
-        "tar -zxvf helm-v3.0.0-alpha.2-linux-amd64.tar.gz",
-        "mv linux-amd64/helm /go/bin/",
-        "helm init",
-        "helm repo update",
-        "helm install my-redis stable/redis",
-        "kubectl get pods --all-namespaces",
-        "kubectl get pods -w"
+        // "wget https://get.helm.sh/helm-v3.0.0-alpha.2-linux-amd64.tar.gz",
+        // "tar -zxvf helm-v3.0.0-alpha.2-linux-amd64.tar.gz",
+        // "mv linux-amd64/helm /go/bin/",
+        // "helm init",
+        // "helm repo add brigade https://brigadecore.github.io/charts",
+        // "helm install brigade brigade/brigade",
+        // "kubectl get pods --all-namespaces",
+        // "wget https://github.com/brigadecore/brigade/releases/download/v1.1.0/brig-linux-amd64",
+        // "mv ./brig-linux-amd64 bin/brig && chmod +x bin/brig"
+
+
+        "git clone https://github.com/brigadecore/brigade",
+        "cd brigade",
+        "make e2e"
     );
 
     return kind;
